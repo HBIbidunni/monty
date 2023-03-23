14a3ce2b81824c45d710a05ae2650e00a7e6f397
HBIbidunni <ayodunibidunni@gmail.com>
2023-03-23 12:34:25 -0700

    monty team tasks

--- monty_main.c
+++ monty_main.c
@@ -1,15 +1,9 @@
 #include "monty.h"
-#include <stdio.h>
-#define _GNU_SOURCE
-#include <stdlib.h>
-
 bus_t bus = {NULL, NULL, NULL, 0};
-
 /**
-* main - function for monty code interpreter
-* @argc: argument count
-* @argv: argument value
-*
+* main - monty code interpreter
+* @argc: number of arguments
+* @argv: monty file location
 * Return: 0 on success
 */
 int main(int argc, char *argv[])
